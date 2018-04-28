3a6fb5e3b3a2b297b375952b32350155fed0d6e2
William Goulart <williamgoulart1@hotmail.com>
2018-04-28 19:23:26 -0400

    [UPDATE] :hammer: App

--- applications/starbase/src/App.vue
+++ applications/starbase/src/App.vue
@@ -1,16 +1,28 @@
 <template>
  <div class="section">
-   <p> {{ text }} </p>
+   <h4 class="title"> {{ text }} </h4>
+   <Person
+        v-for="(id, index) in initial_ids"
+        :id="id"
+        :key="index"
+     />
  </div>
 </template>
 
 <script>
+import Person from "./components/Person.vue"
+
 export default {
-  name: 'app',
+  name: "app",
   data() {
     return {
-      text: 'Hello Folks'
+      text: "Hello Folks",
+      id: {},
+      initial_ids: [1, 13, 18, 22]
     }
+  },
+  components: {
+    Person
   }
 }
 </script>
