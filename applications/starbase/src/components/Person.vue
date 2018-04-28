cea47f39ec6c8fdaf4224cb5c0ea2ec5341aef14
William Goulart <williamgoulart1@hotmail.com>
2018-04-28 19:22:13 -0400

    [ADD] Create Person Component Vue

---
+++ applications/starbase/src/components/Person.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="box" @click="switchPerson()">
+    {{ person }}
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: ["id"],
+  data() {
+    return {
+      person: {}
+    }
+  },
+  methods: {
+    fetchPeople(id) {
+      fetch(`https://swapi.co/api/people/${id}/`, {
+        method: "GET"
+      })
+        .then(response => response.json())
+        .then(json => {
+          this.person = json
+        })
+    },
+    switchPerson() {
+      let random_id = Math.floor(Math.random() * 83) + 1
+      this.fetchPeople(random_id)
+    }
+  },
+  created() {
+    this.fetchPeople(this.id)
+  }
+}
+</script>
