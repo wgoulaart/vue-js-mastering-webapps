854fb4997acf719c35d0c973df37bb8455acf1b4
William Goulart <williamgoulart1@hotmail.com>
2018-05-23 21:49:22 -0400

    [UPDATE] Add router-link to Routes :anchor:

--- applications/starbase-router/src/App.vue
+++ applications/starbase-router/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
     <h3>Startbase Router</h3>
+    <h4>
+      <router-link to="/data/people">Meet the Characters</router-link> |
+      <router-link to="/data/planets">Explore the Planets</router-link>
+    </h4>
     <router-view></router-view>
   </div>
 </template>
