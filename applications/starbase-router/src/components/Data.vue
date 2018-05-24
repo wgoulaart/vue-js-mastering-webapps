f372c60b78a9c403aba54b5c2bf8ad72b68f8da9
William Goulart <williamgoulart1@hotmail.com>
2018-05-23 22:27:44 -0400

    [ADD] Create FetchItems via API and add items

--- applications/starbase-router/src/components/Data.vue
+++ applications/starbase-router/src/components/Data.vue
@@ -8,16 +8,31 @@
   export default {
     data() {
       return {
-        type: this.$route.params.type
+        type: this.$route.params.type,
+        items: []
       }
     },
     watch: {
       '$route': 'change'
     },
     methods: {
-      change() {
-        this.type = this.$route.params.type
+      fetchItems() {
+          this.items = []
+          let initial_ids = [13, 18, 22]
+
+          for (let i in initial_ids ) {
+            let id = initial_ids[i]
+
+            fetch(`https://swapi.co/api/${this.type}/${id}/`, {
+              method: "GET"
+            })
+            .then(reponse => response.json())
+            .then(json => this.items.push(json))
+          }
       }
+    },
+    created() {
+      this.fetchItems()
     }
   }
 </script>
