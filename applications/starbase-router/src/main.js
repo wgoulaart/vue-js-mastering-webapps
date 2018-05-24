0d7dd9d1e2e2ff01163573c676983a38f3ec8882
William Goulart <williamgoulart1@hotmail.com>
2018-05-23 21:30:50 -0400

    [UPDATE] Use VueRouter and Create Router

--- applications/starbase-router/src/main.js
+++ applications/starbase-router/src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
 import App from './App.vue'
+import Data from './components/Data.vue'
+
+const routes = [{
+    path: '/data/:type',
+    component: Data.vue
+}]
+
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
   el: '#app',
+  router,
   render: h => App
 })
