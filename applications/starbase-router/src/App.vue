36392b392c108202f99666cbeec517f2cd2350da
William Goulart <williamgoulart1@hotmail.com>
2018-05-23 22:25:31 -0400

    [UPDATE] Add class to style via css :nails:

--- applications/starbase-router/src/App.vue
+++ applications/starbase-router/src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <h3>Startbase Router</h3>
     <h4>
-      <router-link to="/data/people">Meet the Characters</router-link> |
-      <router-link to="/data/planets">Explore the Planets</router-link>
+      <router-link class="category" to="/data/people">Meet the Characters</router-link> |
+      <router-link class="category" to="/data/planets">Explore the Planets</router-link>
     </h4>
     <router-view></router-view>
   </div>
