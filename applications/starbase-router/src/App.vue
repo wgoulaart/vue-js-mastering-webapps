1d7913e1927dea88b0aee3fd93dba3e62cf8c4b2
William Goulart <williamgoulart1@hotmail.com>
2018-05-28 23:50:03 -0400

    [UPDATE] Change paths css & Text

--- applications/starbase-router/src/App.vue
+++ applications/starbase-router/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h3>Startbase Router</h3>
+    <h3 class="title">Startbase Router</h3>
     <h4>
       <router-link class="category" to="/data/people">Meet the Characters</router-link> |
       <router-link class="category" to="/data/planets">Explore the Planets</router-link>
