891c2ba19ccac6fed0e117f187298b94d71f467f
William Goulart <williamgoulart1@hotmail.com>
2018-06-12 00:56:37 -0400

    [ADD] Vuex include and create Mutations & Actions

--- applications/jokester/src/components/App.vue
+++ applications/jokester/src/components/App.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="app">
-    Let's go jokes?
-    {{ $store.state }}
+    <h3 class="title is-3">Let's go jokes?</h3>
+    <button class="button is-primary" @click="initJokes">Add random Jokes</button>
+    <div class="subtitle">
+      {{ $store.state }}
+    </div>
   </div>
 </template>
+<script>
+  import { mapActions } from 'vuex'
+
+  export default {
+    methods: mapActions([
+      'initJokes'
+    ])
+  }
+</script>
--- applications/jokester/src/store/mutations.js
+++ applications/jokester/src/store/mutations.js
@@ -1,4 +1,4 @@
-import * as types from './mutations-types'
+import * as types from './mutation-types'
 
 export const mutations = {
   [types.INIT_JOKES] (state, payload) {
