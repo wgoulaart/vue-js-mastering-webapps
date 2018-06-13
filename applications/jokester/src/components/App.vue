29c9dc30dc017447a1472b8100313e6978ba563e
William Goulart <williamgoulart1@hotmail.com>
2018-06-13 15:57:34 -0400

    [NEW] Joke component :black_joker:

--- applications/jokester/src/components/App.vue
+++ applications/jokester/src/components/App.vue
@@ -2,17 +2,26 @@
   <div id="app">
     <h3 class="title is-3">Let's go jokes?</h3>
     <button class="button is-primary" @click="initJokes">Add random Jokes</button>
-    <div class="subtitle">
-      {{ $store.state }}
+    <div class="column">
+      <Joke
+        v-for="(joke, index) in $store.state.jokes"
+        :joke="joke"
+        key="index"
+      />
     </div>
+  
   </div>
 </template>
 <script>
   import { mapActions } from 'vuex'
+  import Joke from './Joke.vue'
 
   export default {
     methods: mapActions([
       'initJokes'
-    ])
+    ]),
+    components: {
+      Joke
+    }
   }
 </script>
---
+++ applications/jokester/src/components/Joke.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="column">
+    <div class="joke-card">
+      <div class="card-content">
+        <h4 class="title is-4"> {{joke.setup}} </h4>
+        <p class="subtitle"> {{joke.punchline}} </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+      props: ['joke']
+    }
+</script>
