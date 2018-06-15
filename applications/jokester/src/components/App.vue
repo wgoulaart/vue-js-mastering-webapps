459bc5121c25b29bda2f752d9a04adf6375c66ff
William Goulart <williamgoulart1@hotmail.com>
2018-06-15 01:42:02 -0400

    [ADD] Filtering Jokes :rocket:

--- applications/jokester/src/components/App.vue
+++ applications/jokester/src/components/App.vue
@@ -3,12 +3,26 @@
     <h3 class="title is-3">Let's go jokes?</h3>
     <button class="button is-primary" @click="initJokes">Add random Jokes</button>
     <button class="button is-primary" @click="addJoke">Add New Joke</button>
+    <br>
+    <span class="subtitle" v-for="type in types">
+      <label class="checkbox">
+        <input
+          type="checkbox"
+          class="checkbox"
+          :value="type"
+          v-model="checkedTypes"
+          checked>
+        {{ type }} &nbsp;
+      </label>
+    </span>
+
     <div class="column">
       <Joke
         v-for="(joke, index) in $store.state.jokes"
         :index="index"
         :joke="joke"
         key="index"
+        v-show="checkedTypes.includes(joke.type)"
       />
     </div>
 
@@ -19,6 +33,12 @@
   import Joke from './Joke.vue'
 
   export default {
+    data() {
+      return {
+        types: ['general', 'knock-knock', 'programming'],
+        checkedTypes: ['general', 'knock-knock', 'programming'],
+      }
+    },
     methods: mapActions([
       'initJokes',
       'addJoke'
