2549421c16734b6f2798cc87b6e17bf93dc41772
William Goulart <williamgoulart1@hotmail.com>
2018-06-17 04:13:04 -0400

    [UPDATE] Create component SingIn with Auth on firebase :electric_plug:

--- applications/turnout/src/components/SingIn.vue
+++ applications/turnout/src/components/SingIn.vue
@@ -1,3 +1,50 @@
 <template>
-  <h4 class="title is-4">SingIn Component</h4>
+  <div id="app">
+    <h4 class="title is-4">SingIn</h4>
+    <div class="form">
+      <input
+        type="text"
+        placeholder="E-mail"
+        class="input"
+        v-model="email"
+      />
+      <input
+        type="password"
+        placeholder="password"
+        class="input"
+        v-model="password"
+      />
+      <button class="button is-primary" @click="singIn">Sing In</button>
+    </div>
+
+    <br>
+
+    <p class="subtitle is-3"> {{ error.message}} </p>
+  </div>
 </template>
+
+<script>
+import { firebaseApp } from '../firebaseApp.js'
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      error: {
+        message: ''
+      }
+    }
+  },
+  methods: {
+    singIn() {
+      firebaseApp
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .catch(error => {
+          this.error = error
+        })
+    }
+  }
+}
+</script>
